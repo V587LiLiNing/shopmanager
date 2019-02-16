1b534b10843776513c4846e77a50eb0b1fde3c52
‘ <1761850164@qq.com>
2019-02-16 22:52:22 +0800

    登录验证

--- src/components/login.vue
+++ src/components/login.vue
@@ -15,17 +15,17 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       formDate: {
-        username: "",
-        password: ""
+        username: '',
+        password: ''
       }
-    };
+    }
   },
   methods: {
     // 登录请求
-    async handleLogin() {
+    async handleLogin () {
       // console.log(1)
       // 前提: api-server-> node app.js
       // 服务器支持跨域(api-server和项目代码不放在一块也可以)
@@ -34,29 +34,29 @@ export default {
       // 1.url写错
       // 2. axios导入失败
       // 3.客户端一点问题没有->服务器卡死=重启服务器
-      const res = await this.$http.post(`login`, this.formDate);
+      const res = await this.$http.post(`login`, this.formDate)
       // console.log(res);
       const {
         data: {
-          data: { token },
+          data,
           meta: { msg, status }
         }
-      } = res;
+      } = res
       if (status === 200) {
         // 把正确的用户的token保存起来
         // 存值
-        localStorage.setItem("token", token);
+        localStorage.setItem('token', data.token)
         // 取值
         // const aa = localStorage.getItem("token");
         // console.log(aa);
 
         // 渲染home组件  =改标识 = js改标识
         this.$router.push({
-          name: "home"
-        });
+          name: 'home'
+        })
       } else {
         // 提示框
-        this.$message.error(msg);
+        this.$message.error(msg)
       }
       /*
         .then(res => {
@@ -83,7 +83,7 @@ export default {
         */
     }
   }
-};
+}
 </script>
 
 <style>
