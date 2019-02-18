b72e1344a97b87c29811a672d21dd0dc2012b88e
‘ <1761850164@qq.com>
2019-02-18 11:23:58 +0800

    slot-scope日期格式

--- src/components/users.vue
+++ src/components/users.vue
@@ -20,11 +20,29 @@
     </el-row>
     <!-- 表格 -->
     <el-table :data="list" style="width: 100%">
-      <el-table-column prop="date" label="#" width="80"></el-table-column>
-      <el-table-column prop="date" label="姓名" width="120"></el-table-column>
-      <el-table-column prop="date" label="邮箱" width="140"></el-table-column>
-      <el-table-column prop="date" label="电话" width="140"></el-table-column>
-      <el-table-column prop="date" label="创建日期" width="140"></el-table-column>
+      <el-table-column prop="id" label="#" width="80"></el-table-column>
+      <el-table-column prop="username" label="姓名" width="120"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="140"></el-table-column>
+      <el-table-column prop="mobile" label="电话" width="140"></el-table-column>
+
+      <!-- 日期格式处理->过滤器-> 2类+3步 -->
+      <!-- fmtdate的使用 -->
+      <!-- 1. v-bind:  -->
+      <!-- 2. {{msg | fmtdate}} -->
+
+      <!-- 思路1 尝试prop="create_time | fmtdate"  不行prop不支持 内部写过滤器 -->
+      <!-- 思路2 可以渲染日期格式 但是会报错 , 不行  -->
+      <!-- 思路3 msg不能是prop内容 否则就会报错 以下思路 -->
+        <!-- 1. 给单元格外层加template -->
+        <!-- 2. 给template 设置slot-scope
+        3. slot-scope的值会自动绑定外层数据结构 这里是el-table的data的值list 
+        4. 在内容位置写scope.row 自动取出list中每个对象 其中的row 固定写法-->
+      <el-table-column label="创建日期" width="140">
+        <template slot-scope="scope">
+          {{scope.row.create_time | fmtdate}}
+        </template>
+      </el-table-column>
+
       <el-table-column prop="date" label="用户状态" width="140"></el-table-column>
       <el-table-column prop="date" label="禁用" width="200"></el-table-column>
     </el-table>
@@ -60,7 +78,14 @@ export default {
           this.pagesize
         }`
       )
-      console.log(res)
+      // 解构赋值来使用数据
+      // console.log(res)
+      const {data, meta: {msg, status}} = res.data
+      if(status === 200) {
+        this.list = data.users;
+        console.log(this.list);
+        
+      }
     }
   }
 }
